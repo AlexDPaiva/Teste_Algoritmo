665a8d4535878117a9394bbe9a33c4f89a8eaa02
AlexDPaiva <alex.rl.paiva@gmail.com>
2020-12-04 01:10:23 -0300

    Mudei o arquivo React

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
+import React from 'react';
+
+import Header from './componentes/header';
+
 
 function App() {
   
-  function logar(){
-    
-  }
   return (
     <div className="App">
       
-      <label>Login</label><br></br>
-      <input id="userLogin"></input><br></br>
-      <label>Senha</label><br></br>
-      <input id="userPass" type = "password"></input><br></br>
-      <button onClick = {logar} >LOGAR</button>
+      <Header></Header>
     </div>
   );
 }
