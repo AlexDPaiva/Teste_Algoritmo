92bbd7d2dd5a28386413b16b52552ee018bdd3dc
AlexDPaiva <alex.rl.paiva@gmail.com>
2020-12-04 01:35:00 -0300

    Mudei o arquivo React

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 
- export default function Header(){
+  function Header(){
     return <div>
         <h1> Olá Mundo no React !!! </h1>
     </div>
 };
+export default Header()
